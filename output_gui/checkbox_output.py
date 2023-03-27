a6b79d0a3f7330e5864efa81e712a651496de223
evgeniy <pavlinl4@gmail.com>
2023-03-27 23:02:32 +0300

    i hope it's good with window size and words count

--- output_gui/checkbox_output.py
+++ output_gui/checkbox_output.py
@@ -2,30 +2,28 @@ import tkinter as tk
 
 
 def create_checkbox_list(words):
-
     def submit():
-        # selected_words.append([word for i, word in enumerate(words) if checkboxes[i].get() == 1])
-        # selected_words = [word for i, word in enumerate(words) if checkboxes[i].get() == 1]
         for i, word in enumerate(words):
             if checkboxes[i].get() == 1:
                 selected_words.append(word)
-
         window.destroy()
-        # return selected_words
 
+    h = 30 * (len(words) % 10 + 10)
+    l = 200 + 140 * ((len(words) // 10))
     window = tk.Tk()
-    window.geometry("300x200")
+    window.geometry(f"{l}x{h}")
     window.title("Checkbox List")
 
     checkboxes = []
     for i, word in enumerate(words):
         var = tk.IntVar()
-        checkbox = tk.Checkbutton(window, text=word, variable=var, onvalue=1, offvalue=0)
-        checkbox.grid(row=i, column=0, padx=5, pady=5)
+        checkbox = tk.Checkbutton(window, text=word.strip(), variable=var, onvalue=1, offvalue=0)
+        checkbox.grid(row=i % 10, column=1 + i // 10, padx=30, pady=5, sticky='W')
         checkboxes.append(var)
 
     submit_button = tk.Button(window, text="Submit", command=submit)
-    submit_button.grid(row=i + 1, column=0, columnspan=len(words), padx=5, pady=5)
+    # submit_button.grid(row=i + 1, column=0, columnspan=len(words), padx=50, pady=5)
+    submit_button.grid(row=i + 1, column=0, columnspan=len(words), pady=35)
 
     window.mainloop()
     return selected_words
@@ -33,7 +31,11 @@ def create_checkbox_list(words):
 
 # Example usage
 
-words = ["apple", "banana", "cherry", "durian"]
+
 selected_words = []
-_words = create_checkbox_list(words)
+_words = create_checkbox_list(
+    ["industrialisation", "apple", 'melon', 'fox', 'rabbit', 'box', 'knife', 'bread', 'fox', 'rabbit', 'box', 'knife',
+     'bread', 'fox', 'rabbit', 'box', 'knife', 'bread', 'fox', 'rabbit', 'box', 'knife', 'bread', "industrialisation",
+     "industrialisation", "apple", 'melon', 'fox', 'rabbit', 'box', 'knife', 'bread', 'fox', 'rabbit', 'box', 'knife',
+     'bread', 'fox', 'rabbit', 'box', 'knife', 'bread', 'fox', 'rabbit', 'box'])
 print(_words)
